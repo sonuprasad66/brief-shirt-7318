e2e25d373f38bd83b1239e7cba00f4879106d38a
Sonu Prasad <sp.srp1999@gmail.com>
2022-08-24 12:43:36 +0530

    Clockify Clone

--- clockify_website/src/Components/Home/FreeTimeTracker.jsx
+++ clockify_website/src/Components/Home/FreeTimeTracker.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import styles from "../Styles/HomeCss/FreeTimeTracker.module.css";
 
 export const FreeTimeTracker = () => {
-  return <div>FreeTimeTracker</div>;
+  return (
+    <div className={styles.Container}>
+      <h2>FreeTimeTracker</h2>
+    </div>
+  );
 };
